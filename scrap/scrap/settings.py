9f9aa05514b1bdd656bad9e1df94da172dabbbf7
R-Krishnan21 <59525445+R-Krishnan21@users.noreply.github.com>
2021-01-18 18:23:38 +0530

    Update settings.py

--- scrap/scrap/settings.py
+++ scrap/scrap/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fss=6%2g4+wxm&z6+v^%=ee5xdl&!r#4601nm-%oegvb72ooh#'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
